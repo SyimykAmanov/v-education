d2f1585246e65cc112dd302ea510d1c1969ce612
SyimykAmanov <ulanbayuuly@gmail.com>
2026-03-17 21:08:52 +0100

    feat(async) load data in parallel

--- js/data/dataService.js
+++ js/data/dataService.js
@@ -1,14 +1,17 @@
 import { subjects, lessons, faqData} from "./content.js";
 
 export async function getSubjectById(subjectId) {
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
+  if (Math.random() < 0.2) {
+    throw new Error("Server-Fehler: Daten konnten nicht geladen werden.")
+  }
   return subjects.find(s => s.id === subjectId) ?? null;
 }
 
 
 export async function getLessonsBySubjectId(subjectId) {
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
   return lessons
     .filter(lesson => lesson.subjectId === subjectId)
@@ -18,7 +21,7 @@ export async function getLessonsBySubjectId(subjectId) {
 
 
 export async function getLesson(subjectId, lessonId) {
-  await new Promise(resolve => setTimeout(resolve, 2000));
+  await new Promise(resolve => setTimeout(resolve, 1000));
 
   return lessons.find(lesson => lesson.subjectId === subjectId && String(lesson.id) == String(lessonId))
 }
--- js/pages/subjectPage.js
+++ js/pages/subjectPage.js
@@ -3,25 +3,38 @@ import { lessonCard } from "../components/lessonCard.js";
 
 export const subjectPage = {
   async render({ subjectId }) {
-    const subject = await getSubjectById(subjectId);
-    if (!subject) return `<h1>Kein Fach gefunden</h1><p>${subjectId}</p>`;
+    try {
+      const [subject, subjectLessons] = await Promise.all([
+        getSubjectById(subjectId), 
+        getLessonsBySubjectId(subjectId)]);
+        
+      if (!subject) return `<h1>Kein Fach gefunden</h1><p>${subjectId}</p>`;
 
-    const subjectLessons = await getLessonsBySubjectId(subjectId);
-    console.log(subjectLessons)
-    const list = subjectLessons.length
-      ? subjectLessons.map(lessonCard).join("")
-      : `<li class="card lesson-preview"><p class="card__description">Keine Videos verfügbar</p></li>`;
 
-    return `
-      <section class="hero-sections">
-        <h1 class="hero-sections__title">${subject.title}</h1>
-        <p class="hero-sections__subtitle">${subject.subtitle ?? ""}</p>
-      </section>
+      const list = subjectLessons.length
+        ? subjectLessons.map(lessonCard).join("")
+        : `<li class="card lesson-preview"><p class="card__description">Keine Videos verfügbar</p></li>`;
 
-      <section class="lessons-preview">
-        <h2 class="lessons-preview__title">Lektionsliste</h2>
-        <ul class="lessons-preview__items">${list}</ul>
-      </section>
-    `;
+      return `
+        <section class="hero-sections">
+          <h1 class="hero-sections__title">${subject.title}</h1>
+          <p class="hero-sections__subtitle">${subject.subtitle ?? ""}</p>
+        </section>
+
+        <section class="lessons-preview">
+          <h2 class="lessons-preview__title">Lektionsliste</h2>
+          <ul class="lessons-preview__items">${list}</ul>
+        </section>
+      `;
+    } catch (error) {
+      console.error(error);
+        return `
+            <div class=\"error-container\">
+                <h2>Opps! Etwas ist schief gelaufen.</h2>
+                <p>${error.message}</p>
+                <button class="button" onclick=\"window.location.reload()\">Erneut versuchen</button>
+            </div>
+        `;
+    }
   }
 };
